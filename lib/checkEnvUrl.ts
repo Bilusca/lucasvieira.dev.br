5f39645663a68490b7515f577d1340d8d27bd262
Lucas Assis Vieira <lucasav2005@hotmail.com>
2023-01-19 19:18:36 -0300

    fix: production url

--- lib/checkEnvUrl.ts
+++ lib/checkEnvUrl.ts
@@ -1,5 +1,5 @@
 export function checkEnvUrl(): string {
-  const properUrl = process.env.NODE_ENV === 'production' ? 'https://lucasvieira.dev/' : 'http://localhost:3000/'
+  const properUrl = process.env.NODE_ENV === 'production' ? 'https://www.lucasvieira.dev/' : 'http://localhost:3000/'
 
   return properUrl;
 }
