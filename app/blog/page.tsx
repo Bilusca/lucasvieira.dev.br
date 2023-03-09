54c2ef83986a40483f4c636de32b76fe1fb3848f
Lucas Assis Vieira <lucasav2005@hotmail.com>
2023-03-08 21:30:27 -0300

    fix: gap on blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -63,7 +63,7 @@ export default async function BlogListPage() {
   return (
     <main>
       <h1 className="text-center text-shadow-purple font-bold text-6xl lg:text-8xl tracking-tighter mb-20 lg:mb-40">Artigos no site</h1>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-x-10">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-y-5 lg:gap-x-10">
         {blogData.map(post => <BlogCard key={post.id} title={post.title} slug={post.slug} description={post.description} coverImage={post.coverImage} />)}
       </div>
     </main>
